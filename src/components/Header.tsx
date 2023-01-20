be97a96ee0d0db7c08a53161f8d90bd925b1382e
maarcos4g <marcospaullo552@gmail.com>
2023-01-20 00:26:42 -0300

    aula 03: interface de novo hábito

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,9 +3,13 @@ import { Feather } from '@expo/vector-icons';
 
 import colors from 'tailwindcss/colors';
 
+import { useNavigation } from '@react-navigation/native'
+
 import Logo from '../assets/logo.svg';
 
 export function Header() {
+  const { navigate } = useNavigation();
+
   return (
     <View className="w-full flex-row items-center justify-between">
       <Logo />
@@ -13,6 +17,7 @@ export function Header() {
       <TouchableOpacity
         activeOpacity={0.7}
         className="flex-row h-11 px-4 border border-violet-500 rounded-lg items-center"
+        onPress={() => navigate('new')}
       >
         <Feather
           name="plus"
